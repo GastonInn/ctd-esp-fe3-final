88e8d8395fdef2e472abb0f2a7e63679a12bdeb6
GastonInn <gaston.i.0201@gmail.com>
2022-12-10 03:41:08 -0300

    Moving the tests to a proper folder

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,14 +1,12 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from 'react'
 import "@testing-library/jest-dom/extend-expect";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
+import App from '../App';
 import axios from "axios";
-import Contact from "./Routes/Contact";
-import Form from "./Components/Form";
- 
-const fetchData = () => axios.get("https://jsonplaceholder.typicode.com/users/");
+import Contact from "../Routes/Contact";
 
+const fetchData = () => axios.get("https://jsonplaceholder.typicode.com/users/");
 
 test("Testeando si la api funciona correctamente", async () => {
     return fetchData().then(res => {
@@ -21,7 +19,6 @@ test("Testeando si en la pantalla se visualiza el texto Contact del navbar", asy
     const texto = await screen.findByText("Contact")
     expect(texto.textContent).toBe("Contact")
 })
-
 
 test('Testeando si se renderiza el boton de submit del formulario', () => {
     render(<Contact/>);
